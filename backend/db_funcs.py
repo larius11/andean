e2e7ac982f40fc22f285e6e4a1a8660e13403b06
Ricardo Riveron <larius828@utexas.edu>
2019-09-14 14:37:20 -0500

    Fixed imports

--- backend/db_funcs.py
+++ backend/db_funcs.py
@@ -2,7 +2,7 @@ from db_structure import db, SubCategory, Category, Product
 
 
 def insert_product(category, subCategory, color,
-                   name, price: float, details, image):
+                   name, price, details, image):
     print("Beginning to insert...")
     # Search for subCategory
     fk_sub_cat = db.session.query(SubCategory.id).filter_by(name=subCategory).first()
