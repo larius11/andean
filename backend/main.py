27a037221862eb27d6e80d23ef870957123c4805
Ricardo Riveron <larius828@utexas.edu>
2019-09-18 18:58:20 -0500

    bugfixing

--- backend/main.py
+++ backend/main.py
@@ -31,6 +31,7 @@ def get_product():
 @app.route('/products', methods=['GET'])
 def get_products():
     fields = request.json
+    print(request)
     return jsonify(get_some_products(fields['category'], fields['subCategory']))
 
 
